f5e8600e5873c67321a96db3e13f91da64785cee
schaererPanda <114098546+schaererPanda@users.noreply.github.com>
2023-08-14 08:51:12 -0500

    Get emergency departments list from API and make the insert form work

--- public/js/api.js
+++ public/js/api.js
@@ -109,3 +109,33 @@ export async function insertHospitalRegion(data) {
 
   return true;
 }
+
+export async function getEmergencyDepartments() {
+  const response = await fetch(`/api/emergency-departments`);
+  if (!response.ok) {
+    const { message } = await response.json();
+    alert(message);
+    return;
+  }
+
+  return await response.json();
+}
+
+export async function insertEmergencyDepartment(data) {
+  const response = await fetch(`/api/emergency-departments`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) {
+    const { message } = await response.json();
+    alert(message);
+    return;
+  }
+
+  const { message } = await response.json();
+  alert(message);
+
+  return true;
+}
