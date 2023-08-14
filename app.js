e1c5e07f4b23dd1a4a303a9535ad99c486372a88
schaererPanda <114098546+schaererPanda@users.noreply.github.com>
2023-08-13 19:49:30 -0500

    Use the .env file for env variables

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 /*
     SETUP
 */
+const dotenv = require("dotenv");
+dotenv.config();
+
 var express = require("express"); // We are using the express library for the web server
 var app = express(); // We need to instantiate an express object to interact with the server in our code
 const PORT = process.env.PORT || 9124; // Set a port number at the top so it's easy to change in the future
